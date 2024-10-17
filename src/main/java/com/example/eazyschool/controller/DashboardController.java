781aab891cf80234e5b0f35fca30424d26255283
Kamla Kant <kktkamla@gmail.com>
2024-10-17 23:12:26 +0530

    added many more features

--- src/main/java/com/example/eazyschool/controller/DashboardController.java
+++ src/main/java/com/example/eazyschool/controller/DashboardController.java
@@ -1,6 +1,10 @@
 package com.example.eazyschool.controller;
 
+import com.example.eazyschool.model.Person;
+import com.example.eazyschool.repository.PersonRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DashboardController {
 
+    @Autowired
+    PersonRepository personRepository;
+
     @RequestMapping("/dashboard")
-    public String displayDashboard(Model model,Authentication authentication) {
-        model.addAttribute("username", authentication.getName());
+    public String displayDashboard(Model model, Authentication authentication, HttpSession session) {
+        Person person = personRepository.readByEmail(authentication.getName());
+        model.addAttribute("username", person.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
+        if(null != person.getEazyClass() && null != person.getEazyClass().getName()){
+            model.addAttribute("enrolledClass", person.getEazyClass().getName());
+        }
+        session.setAttribute("loggedInPerson", person);
 //        throw new RuntimeException("It's been a bad day!!");
         return "dashboard.html";
     }
--- src/main/java/com/example/eazyschool/repository/HolidaysRepository.java
+++ src/main/java/com/example/eazyschool/repository/HolidaysRepository.java
@@ -1,19 +1,8 @@
 package com.example.eazyschool.repository;
 
-import com.example.eazyschool.model.Contact;
 import com.example.eazyschool.model.Holiday;
-import com.example.eazyschool.rowmappers.ContactRowMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.List;
 
 /*
 @Repository stereotype annotation is used to add a bean of this class
@@ -21,19 +10,6 @@ type to the Spring context and indicate that given Bean is used to perform
 DB related operations and
 * */
 @Repository
-public class HolidaysRepository {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public HolidaysRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public List<Holiday> findAllHolidays() {
-        String sql = "SELECT * FROM holidays";
-        var rowMapper = BeanPropertyRowMapper.newInstance(Holiday.class);
-        return jdbcTemplate.query(sql, rowMapper);
-    }
+public interface HolidaysRepository extends CrudRepository<Holiday, String> {
 
 }
